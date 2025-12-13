294fbe62379e014bb50f7519405b895bbbb76dfb
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-13 20:14:12 -0300

    Criei script DDL para a tabela municipio no diretório banco_de_dados
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela municipio no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

---
+++ banco_de_dados/DDL/Tabelas/05_tbl_municipio.sql
@@ -0,0 +1,15 @@
+-- |===================================================================| 
+-- | Tabela: municipio                                                 |
+-- | Descrição: armazena municípios para organização geográfica        |
+-- | e suporte a análises a nível local.                               | 
+-- | Ordem de criação: quinta tabela a ser criada.                     |                                                               
+-- |===================================================================| 
+
+CREATE TABLE municipio ( id_municipio INT PRIMARY KEY, municipio VARCHAR (50), 
+area_municipio NUMERIC (10,3) NOT NULL, latitude NUMERIC (9,6) NOT NULL, 
+longitude NUMERIC (9,6) NOT NULL, altitude NUMERIC (12,6) NOT NULL, id_microrregiao INT NOT NULL,
+id_mesorregiao INT NOT NULL, id_estado INT NOT NULL,
+FOREIGN KEY (id_microrregiao) REFERENCES microrregiao (id_microrregiao),
+FOREIGN KEY (id_mesorregiao)  REFERENCES mesorregiao (id_mesorregiao),
+FOREIGN KEY (id_estado)  REFERENCES estado (id_estado)
+);
