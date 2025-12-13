1e78f7d64e82a94bef02eed87c054169324de874
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-13 20:23:52 -0300

    Criei script DDL para a tabela bacia_hidrografica no diretório banco_de_dados
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela bacia_hidrografica no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos que compõem as bacias hidrográficas. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

---
+++ banco_de_dados/DDL/Tabelas/14_tbl_bacia_hidrografica.sql
@@ -0,0 +1,9 @@
+-- |===================================================================| 
+-- | Tabela: bacia_hidrografica                                        |
+-- | Descrição: armazena as bacias hidrográficas do Brasil e do mundo. |
+-- | Ordem de criação: décima quarta a ser criada.                     |                   
+-- |===================================================================| 
+
+CREATE TABLE bacia_hidrografica ( id_bacia_hidrografica INT PRIMARY KEY,
+bacia_hidrografica VARCHAR (80) NOT NULL
+);
