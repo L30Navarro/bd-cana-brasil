71f1ab43cda8c8acc64caad93c9009f684b26d05
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-13 20:12:29 -0300

    Criei script DDL para a tabela pais no diretório banco_de_dados
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela pais no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

---
+++ banco_de_dados/DDL/Tabelas/01_tbl_pais.sql
@@ -0,0 +1,8 @@
+-- |===================================================================| 
+-- | Tabela: pais                                                      |
+-- | Descrição: armazena países para organização geográfica            |
+-- | e suporte a análises a nível nacional e internacional.            |                                                        |
+-- | Ordem de criação: primeira tabela a ser criada.                   |                                                               
+-- |===================================================================| 
+
+CREATE TABLE pais ( id_pais INT PRIMARY KEY, pais VARCHAR (80));
