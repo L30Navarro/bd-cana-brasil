f2155b01cd987aa553069a12635cf51a6e9dd989
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-15 15:16:22 -0300

    Atualizei o script DDL para a tabela estado no diretório banco_de_dados, porque faltou a restrição NOT NULL nele.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela estado no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/02_tbl_estado.sql
+++ banco_de_dados/DDL/Tabelas/02_tbl_estado.sql
@@ -1,11 +1,11 @@
 -- |===================================================================| 
 -- | Tabela: estado                                                    |
--- | Descrição: armazena estados para organização geográfica           |
+-- | Descrição: armazena os estados para organização geográfica        |
 -- | e suporte a análises a nível estadual.                            | 
 -- | Ordem de criação: segunda tabela a ser criada.                    |                                                               
 -- |===================================================================| 
 
-CREATE TABLE estado ( id_estado INT PRIMARY KEY, estado VARCHAR (50) NOT NULL, uf CHAR (2), 
+CREATE TABLE estado ( id_estado INT PRIMARY KEY, estado VARCHAR (50) NOT NULL, uf CHAR (2) NOT NULL, 
 id_pais INT NOT NULL,
 FOREIGN KEY (id_pais) REFERENCES pais (id_pais)
 );
