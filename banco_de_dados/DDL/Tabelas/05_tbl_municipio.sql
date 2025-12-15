f7a555448694bd79ec8012e4438d53d909ac5870
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-15 15:22:19 -0300

    Atualizei o script DDL para a tabela municipio no diretório banco_de_dados, porque faltou a restrição NOT NULL nele.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela municipio no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/05_tbl_municipio.sql
+++ banco_de_dados/DDL/Tabelas/05_tbl_municipio.sql
@@ -1,11 +1,11 @@
 -- |===================================================================| 
 -- | Tabela: municipio                                                 |
--- | Descrição: armazena municípios para organização geográfica        |
+-- | Descrição: armazena os municípios para organização geográfica     |
 -- | e suporte a análises a nível local.                               | 
 -- | Ordem de criação: quinta tabela a ser criada.                     |                                                               
 -- |===================================================================| 
 
-CREATE TABLE municipio ( id_municipio INT PRIMARY KEY, municipio VARCHAR (50), 
+CREATE TABLE municipio ( id_municipio INT PRIMARY KEY, municipio VARCHAR (50) NOT NULL, 
 area_municipio NUMERIC (10,3) NOT NULL, latitude NUMERIC (9,6) NOT NULL, 
 longitude NUMERIC (9,6) NOT NULL, altitude NUMERIC (12,6) NOT NULL, id_microrregiao INT NOT NULL,
 id_mesorregiao INT NOT NULL, id_estado INT NOT NULL,
