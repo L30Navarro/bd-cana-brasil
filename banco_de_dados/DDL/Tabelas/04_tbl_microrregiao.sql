51c9f72a19d1d356534df86992004a256261d0f6
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-15 15:19:12 -0300

    Atualizei o script DDL para a tabela microrregiao no diretório banco_de_dados, porque faltou a restrição NOT NULL nele.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela microrregiao no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/04_tbl_microrregiao.sql
+++ banco_de_dados/DDL/Tabelas/04_tbl_microrregiao.sql
@@ -5,7 +5,7 @@
 -- | Ordem de criação: quarta tabela a ser criada.                     |                                                               
 -- |===================================================================| 
 
-CREATE TABLE microrregiao ( id_microrregiao INT PRIMARY KEY, microrregiao VARCHAR (40),
-area_microrregiao NUMERIC (9,3), id_mesorregiao INT NOT NULL,
+CREATE TABLE microrregiao ( id_microrregiao INT PRIMARY KEY, microrregiao VARCHAR (40) NOT NULL,
+area_microrregiao NUMERIC (9,3) NOT NULL, id_mesorregiao INT NOT NULL,
 FOREIGN KEY (id_mesorregiao) REFERENCES mesorregiao (id_mesorregiao)
 );
