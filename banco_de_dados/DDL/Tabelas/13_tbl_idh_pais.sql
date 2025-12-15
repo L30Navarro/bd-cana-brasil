729eac519cc8ebb107116342a6bef8c119dbb0a7
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-15 15:26:39 -0300

    Atualizei o script DDL para a tabela idh_pais no diretório banco_de_dados, porque faltou a restrição NOT NULL nele.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela idh_pais no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos que compõem o Índice de Desenvolvimento Humano da nação. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/13_tbl_idh_pais.sql
+++ banco_de_dados/DDL/Tabelas/13_tbl_idh_pais.sql
@@ -4,8 +4,9 @@
 -- | Ordem de criação: décima terceira tabela a ser criada.            |                   
 -- |===================================================================| 
 
-CREATE TABLE idh_pais ( id_idh_pais SERIAL PRIMARY KEY,  periodo INT NOT NULL,
-pais_idh_geral NUMERIC (4,3) NOT NULL, idh_pais_renda NUMERIC (4,3),
-idh_pais_longevidade NUMERIC (4,3), idh_pais_educacao NUMERIC (4,3), id_pais INT NOT NULL,
+CREATE TABLE idh_pais ( id_idh_pais SERIAL PRIMARY KEY, periodo INT NOT NULL,
+pais_idh_geral NUMERIC (4,3) NOT NULL, idh_pais_renda NUMERIC (4,3) NOT NULL,
+idh_pais_longevidade NUMERIC (4,3) NOT NULL, idh_pais_educacao NUMERIC (4,3) NOT NULL, 
+id_pais INT NOT NULL,
 FOREIGN KEY (id_pais) REFERENCES pais (id_pais)
 );
