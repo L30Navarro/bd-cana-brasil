baf229b3643dda8b04d8200a2f662d57f4c05768
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-17 16:15:26 -0300

    Atualizei o script DDL para a tabela estado no diretório banco_de_dados, já que houve mudanças no tamanho do comprimento do atributo "estado" de 50 para 23.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela estado no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos para identificação geográfica. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/02_tbl_estado.sql
+++ banco_de_dados/DDL/Tabelas/02_tbl_estado.sql
@@ -5,7 +5,7 @@
 -- | Ordem de criação: segunda tabela a ser criada.                    |                                                               
 -- |===================================================================| 
 
-CREATE TABLE estado ( id_estado INT PRIMARY KEY, estado VARCHAR (23) NOT NULL, uf CHAR (2) NOT NULL, 
-id_pais INT NOT NULL,
+CREATE TABLE estado ( id_estado INT PRIMARY KEY, estado VARCHAR (23) NOT NULL, 
+uf CHAR (2) NOT NULL, id_pais INT NOT NULL,
 FOREIGN KEY (id_pais) REFERENCES pais (id_pais)
 );
