35582bb54752c0a7eadb088509c18d87a16926e2
L30Navarro <leonardoaugts.navarro@gmail.com>
2025-12-17 16:24:10 -0300

    Atualizei o script DDL para a tabela usina no diretório banco_de_dados, já que houve mudanças nos tamanhos dos comprimentos dos atributos "grupo_empresarial" de 130 para 70 e "unidade" de 70 para 50.
    Este script contém a linguagem DDL (CREATE TABLE) para criar a tabela usina no banco de dados cana_de_acucar.
    Dessa forma, define  a chave primária e os atributos básicos que integram a sua produção, além de identificações geográficas. Assim, é viável implementar a modelagem definida pela regra de negócios nesse banco de dados.

--- banco_de_dados/DDL/Tabelas/07_tbl_usina.sql
+++ banco_de_dados/DDL/Tabelas/07_tbl_usina.sql
@@ -5,8 +5,8 @@
 -- | Ordem de criação: sétima tabela a ser criada.                     |                   
 -- |===================================================================| 
 
-CREATE TABLE usina ( id_usina SERIAL PRIMARY KEY, grupo_empresarial VARCHAR (130) NULL,
-unidade VARCHAR (70) NOT NULL, cnpj CHAR (14) NOT NULL, safra INT NOT NULL,
+CREATE TABLE usina ( id_usina SERIAL PRIMARY KEY, grupo_empresarial VARCHAR (70) NULL,
+unidade VARCHAR (50) NOT NULL, cnpj CHAR (14) NOT NULL, safra INT NOT NULL,
 cap_etanol_anidro INT NOT NULL, cap_etanol_hidratado INT NOT NULL, cap_moagem INT NULL,
 latitude NUMERIC (9,6) NOT NULL, Longitude NUMERIC (9,6) NOT NULL, id_municipio INT NOT NULL,
 FOREIGN KEY (id_municipio) REFERENCES municipio (id_municipio)
